2aaaa603a01aec7e5a9f577d7929fff8b20fc560
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2017-02-08 14:08:58 +0200

    routes

--- js/main.js
+++ js/main.js
@@ -12,5 +12,21 @@ app.config(function($routeProvider, $locationProvider) {
   .when('/features', {
     templateUrl: 'features.html',
     // controller: 'ChapterController'
+  })
+  .when('/get-involved', {
+    templateUrl: 'get-involved.html',
+    // controller: 'ChapterController'
+  })
+  .when('/devices', {
+    templateUrl: 'devices.html',
+    // controller: 'ChapterController'
+  })
+  .when('/team', {
+    templateUrl: 'team.html',
+    // controller: 'ChapterController'
+  })
+  .when('/technology', {
+    templateUrl: 'technology.html',
+    // controller: 'ChapterController'
   });
 });
